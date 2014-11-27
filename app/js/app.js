589cb269ba867dd9ea848c47149fe6d10653041a
r1N0Xmk2 <rene7297152@gmail.com>
2014-11-27 23:17:19 +0800

    add kan

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,30 @@
 var myApp = angular.module('myApp', [
-	'ngRoute'
+	'ngRoute',
+	'angular.filter'
 ])
 myApp
 	.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 		$routeProvider.
 			when('/', {
+				templateUrl: 'view/main.html',
+				controller: 'mainCtrl'
+			}).
+			when('/ensei', {
 				templateUrl: 'view/ensei.html',
-				controller: 'enseiCtrl'
+				controller: 'esCtrl'
+			})
+			.when('/kanmusu', {
+				templateUrl: 'view/kanmusu.html',
+				controller: 'kmCtrl'
 			}).
 			otherwise({
 				redirectTo: '/'
 			})
 	}])
-	.controller('enseiCtrl', ['$scope', 'getJson', function($scope, getJson) {
+	.controller('mainCtrl', ['$scope', function($scope) {
+		
+	}])
+	.controller('esCtrl', ['$scope', 'getJson', function($scope, getJson) {
 		$scope.perHour = false;
 		$scope.dskt = $scope.dsk==true?1.5:1;
 		function esph(object, type, time) {
@@ -26,10 +38,9 @@ myApp
 			enseiOp = [];
 			$scope.ensei.forEach(function(item,index) {
 				item.regardph = JSON.parse(JSON.stringify(item.regard));
-				console.log('calph',$scope.dskt)
-				item.regardph=item.regardph.map(function(e, i) {
+				item.regardph = item.regardph.map(function(e, i) {
 					return esph(item, i, $scope.dskt)
-				}) 
+				})
 			})
 		};
 		$scope.applys = function() {
@@ -55,11 +66,18 @@ myApp
 				y = enseiOp.indexOf($scope.es2);
 				z = enseiOp.indexOf($scope.es3);
 				$scope.total = $scope.total.map(function(e,i) {
-					return ($scope.esh)*((x == -1 ?0:esph($scope.ensei[x], i, $scope.esd1?1.5:1)) 
-															+(y == -1 ?0:esph($scope.ensei[y], i, $scope.esd2?1.5:1)) 
+					return ($scope.esh)*((x == -1 ?0:esph($scope.ensei[x], i, $scope.esd1?1.5:1))
+															+(y == -1 ?0:esph($scope.ensei[y], i, $scope.esd2?1.5:1))
 															+(z == -1 ?0:esph($scope.ensei[z], i, $scope.esd3?1.5:1)))
 				})
 			}
+		})
+	}])
+	.controller('kmCtrl', ['$scope', '$filter', 'getJson', function($scope, $filter, getJson) {
+		getJson.fetch('kansen.json').then(function(data) {
+			console.log(data);
+			
+			$scope.kans = data.api_mst_ship;
 		})
 	}])
 	.factory('getJson', function($q, $timeout, $http) {
@@ -75,4 +93,49 @@ myApp
 			}
 		}
 		return getJson;
+	})
+	
+	.filter('stype', function () {
+	    return function (n) {
+	    	stype = [
+	    						'', 
+	    						'', 
+	    						'駆逐艦', 
+	    						'軽巡洋艦', 
+	    						'重雷装巡洋艦', 
+	    						'重巡洋艦', 
+	    						'航空巡洋艦', 
+	    						'軽空母', 
+	    						'巡洋戦艦', 
+	    						'戦艦', 
+	    						'航空戦艦', 
+	    						'正規空母', 
+	    						'', 
+	    						'潜水艦', 
+	    						'潜水空母', 
+	    						'', 
+	    						'水上機母艦', 
+	    						'揚陸艦', 
+	    						'装甲空母', 
+	    						'工作艦', 
+	    						'潜水母艦'];
+	      return stype[n];
+	    }
+	})
+	.filter('maxeq', function () {
+	    return function (arr) {
+	    	var i = arr.indexOf(0);
+	    	var arr_=arr.map(function(e){return e})
+	    	i=i==0?1:i;
+	    	arr_.splice(i,arr.length-i)
+
+	    	return arr_.join(',');
+	      // return arr==undefined?0:arr;
+	    }
+	})
+	.filter('soku', function () {
+	    return function (n) {
+	    	if(n==5) return '低';
+	    	else if (n==10) return '高';
+	    }
 	});
