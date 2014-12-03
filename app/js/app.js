37e5ec55e90056780e5cf353c1fe9099d1e12940
r1n0xmk2 <rene7297152@gmail.com>
2014-12-03 18:19:53 +0800

    edit vr

--- app/js/app.js
+++ app/js/app.js
@@ -250,4 +250,74 @@ myApp
 			})
 			$scope.filterType();
 		}
+	}])
+	.controller('vrCtrl', ['$scope', 'getJson', 'eqType', 'toSelectors', function($scope, getJson, eqType, toSelectors) {
+		getJson.fetch('Equipment.json').then(function(data) {
+			$scope.equipments = data.api_mst_slotitem;
+			console.log($scope.equipments)
+			$scope.team = [0,0,0,0,0,0];
+			$scope.admiral = 1;
+			var eqArray = [7,8,9,10,11,12,13,29];
+			$scope.eqs = {}
+			eqArray.forEach(function(e) {
+				$scope.eqs[e] = [
+						{
+							"eqid": -1,
+							"num": 1
+						}
+					]
+			})
+			$scope.options = {};
+			console.log($scope.eqArray)
+			eqArray.forEach(function(e) {
+				$scope.options[e] = filterEq(e)
+			})
+			console.log($scope.options)
+			$scope.addItem = function(n) {
+				console.log(n)
+				$scope.eqs[n].push({
+					"eqid": -1,
+					"num": 1
+				})
+				console.log($scope.eqs)
+			}
+			var filterEq = function(n) {
+				var selects = ['選擇裝備'];
+				$scope.equipments.forEach(function(e) {
+					if(e.api_type === n) {
+						selects.push(e);
+					}
+				})
+				return selects;
+			}
+			$scope.calvr = function() {
+			console.log($scope.team)
+			var time = 
+			vrmin = $scope.admiral / 5.0;
+			times = {
+				"7" : [1.0376255,0.09650285],
+				"8" : [1.3677954,0.10863618],
+				"9" : [1.6592780,0.09760553],
+				"10" : [2.0000000,0.08662294],
+				"11" : [1.7787282,0.09177225],
+				"12" : [1.0045358,0.04927736],
+				"13" : [0.9906638,0.04912215],
+				"29" : [0.9067950,0.06582838],
+				"team" : [1.6841056,0.07815942],
+				"admiral" : [-0.6142467,0.03692224],
+			}
+			// 索敵スコア[小数点第2位を四捨五入]
+			// 	= 7艦上爆撃機 × (1.0376255 ± 0.09650285)
+			// 	+ 8艦上攻撃機 × (1.3677954 ± 0.10863618)
+			// 	+ 9艦上偵察機 × (1.6592780 ± 0.09760553)
+			// 	+ 10水上偵察機 × (2.0000000 ± 0.08662294)
+			// 	+ 11水上爆撃機 × (1.7787282 ± 0.09177225)
+			// 	+ 12小型電探 × (1.0045358 ± 0.04927736)
+			// 	+ 13大型電探 × (0.9906638 ± 0.04912215)
+			// 	+ 29探照灯 × (0.9067950 ± 0.06582838)
+			// 	+ √(各艦毎の素索敵) × (1.6841056 ± 0.07815942)
+			// 	+ (司令部レベルを5の倍数に切り上げ) × (-0.6142467 ± 0.03692224)
+		}
+		})
+		
 	}]);
